6ffa3a3595b2a7548ffc306123741f5248189027
Leelavathi D h <leelavathid.h@hcl-software.com>
2026-07-08 14:12:23 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,35 +1,19 @@
 const fs = require("fs");
 const path = require("path");
-const { execFile } = require("child_process");
 
 async function readFile(file) {
-
     const uploadsDir = path.resolve(__dirname, "uploads");
     const resolvedPath = path.resolve(uploadsDir, file);
 
-    if (!resolvedPath.startsWith(uploadsDir)) {
+    if (
+        resolvedPath !== uploadsDir &&
+        !resolvedPath.startsWith(uploadsDir + path.sep)
+    ) {
         throw new Error("Invalid file path");
     }
 
     const data = await fs.promises.readFile(resolvedPath);
-
     console.log(data.toString());
-}
-
-function execute() {
-    execFile("ls", ["-l"], (err, stdout) => {
-
-        if (err) {
-            console.error(err);
-            return;
-        }
-
-        console.log(stdout);
-    });
-}
-
-function unusedMethod() {
-    console.log("never used");
 }
 
 module.exports = {
