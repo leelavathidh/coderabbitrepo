0d43e6aaaa608a630252021d744013aa418cc9c1
leelavathidh <98580118+leelavathidh@users.noreply.github.com>
2026-07-08 10:51:20 +0530

    Implement basic Calculator class with operations

---
+++ calculator.js
@@ -0,0 +1,25 @@
+class Calculator {
+
+    add(a, b) {
+        return a + b;
+    }
+
+    subtract(a, b) {
+        return a - b;
+    }
+
+    multiply(a, b) {
+        return a * b;
+    }
+
+    divide(a, b) {
+        return a / b;
+    }
+
+    addAgain(a, b) {
+        return a + b;
+    }
+
+}
+
+module.exports = Calculator;
